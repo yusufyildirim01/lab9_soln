65e83c3767def74fc45dbe508fd5778785bd8067
yusufyildirim01 <yusuf.lest@gmail.com>
2024-04-30 18:22:00 -0400

    Update midtermpr.ml

---
+++ exam prep/midtermpr.ml
@@ -0,0 +1,35 @@
+
+
+
+
+
+
+let nats_gen () : unit -> int = 
+  let counter = ref ~-1 in 
+  fun () -> incr counter;
+  !counter ;;
+
+
+No. it can mean different things so not pure programming
+
+
+let square_gen : unit -> int =
+  let counter = ref 0 in
+  fun () -> incr counter
+  !counter * !counter ;;
+
+
+let rec stream_of_gen (gen: unit -> 'a) : 'a stream =
+  lazy (Cons (gen (), stream_of_gen gen)) ;;
+
+
+let gen_of_list (lst : 'a list) : unit -> 'a =
+  let store = ref lst in
+  fun () -> match !store with 
+  | [] -> raise NoMore
+  | fst :: tail -> 
+    store := tail; 
+  fst ;;
+
+let x = ref 3 in x := !x * 14
+
